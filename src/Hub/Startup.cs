95c98c48947a345123203898afceab9fed2c3b42
jglozano <javier@lozanotek.com>
2014-09-14 17:58:44 -0700

    Initial commit with all the source.

---
+++ src/Hub/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace yourConf.VideoHub {
+    using Owin;
+
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            // Specify that UserId should be pull from the IIdentity.Name
+            app.MapSignalR();
+        }
+    }
+}
---
+++ src/Hub/VideoHub.cs
@@ -0,0 +1,12 @@
+﻿namespace yourConf.VideoHub
+{
+    using Microsoft.AspNet.SignalR;
+
+    public class VideoHub : Hub
+    {
+        public void Play(string videoId)
+        {
+            Clients.All.PlayVideo(videoId);
+        }
+    }
+}
